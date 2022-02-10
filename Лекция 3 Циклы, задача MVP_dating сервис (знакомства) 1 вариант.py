2f1be0d88fc48c1aa635519241e74a35013c4654
DenisPozdniakov <98995283+DenisPozdniakov@users.noreply.github.com>
2022-02-10 18:53:22 +0300

    Add files via upload
    вариант без использования zip

---
+++ Лекция 3 Циклы, задача MVP_dating сервис (знакомства) 1 вариант.py
@@ -0,0 +1,46 @@
+### Функция добавления новых пользователей
+def add_user():
+  print('Укажите пол пользователя (м/ж): ')
+  gender_user = input().lower()
+  print('Укажите никнейм пользователя на англ расскладке:')
+  nickname_user = input()
+  if gender_user == 'м':
+    boys.append(nickname_user)
+  elif gender_user == 'ж':
+    girls.append(nickname_user)
+  else:
+    print('Вы указали некорректный "Пол", пожалуйста повторите операцию')
+    
+### Основная программа
+boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
+girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
+
+print('Добрый день, вас привествует сервис знакомств, требуется ли добавить новых пользователей? да/нет')
+question_add_users = input().lower()
+if question_add_users == 'да':
+  flag = True
+  while flag == True:
+    add_user()
+    print('Добавить еще пользователя?')
+    question_add_users_into_while = input().lower()
+    if question_add_users_into_while == 'нет':
+      flag = False
+      print('Указанные пользователи добавлены в поиск!')
+
+## Проверяем соразмерность списков
+if len(boys) != len(girls):
+  if len(boys) > len(girls):
+    x1,x2 = 'Мужчин', 'Женщин'
+  else:
+    x2,x1 = 'Мужчин', 'Женщин'
+  print('К сожалению мы не можем подобрать идеальную пару, т.к. количество {0} превышает количество {1}, пожалуйста добавьте пользователей{1}'.format(x1,x2))
+  add_user()
+else:
+  boys.sort()
+  girls.sort()
+  print('Идеальные пары:')
+  for i in range(len(boys)):
+    print(boys[i], 'и', girls[i])
+
+
+
